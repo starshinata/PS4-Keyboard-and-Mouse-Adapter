a7db5fd72acbc3b7cf7ba20f080a103053ff71b7
pancakeslp <pancakeslp@gmail.com>
2020-12-30 01:22:36 +0000

    unit tests for UserSettings ImportValues() to ensure values are imported from json settings file

--- UnitTests/KeyboardAndMouseAdapter/Config/UserSettings/ImportValuesTest.cs
+++ UnitTests/KeyboardAndMouseAdapter/Config/UserSettings/ImportValuesTest.cs
@@ -10,7 +10,8 @@ namespace UnitTests.KeyboardAndMouseAdapter.Config.UserSettingsTest
     [TestClass]
     public class ImportValuesTest
     {
-        private readonly string PROJECT_ROOT = "..\\..\\..\\";
+        private static readonly string PROJECT_ROOT = "..\\..\\..\\";
+        private static readonly string PROFILE_DIRECTORY = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\";
 
         private UserSettings ReadExpectedFile(string file)
         {
@@ -25,41 +26,94 @@ namespace UnitTests.KeyboardAndMouseAdapter.Config.UserSettingsTest
         }
 
         [TestMethod]
-        public void ShouldImport_1_0_11()
+        public void ShouldImport_Profile_1_0_11_Default()
         {
-            string inputFile = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\ImportValues--profile-1.0.11-default--input.json";
-            string expectedFile = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\ImportValues--profile-1.0.11-default--expected.json";
+            string inputFile = PROFILE_DIRECTORY + "ImportValues--profile-1.0.11-default--input.json";
+            string expectedFile = PROFILE_DIRECTORY + "ImportValues--profile-1.0.11-default--expected.json";
             UserSettings.ImportValues(inputFile);
             UserSettings actual = UserSettings.GetInstance();
             actual.KeyboardMappings = null;
             UserSettings expected = ReadExpectedFile(expectedFile);
+
             AssertionExtensions.Should(actual).BeEquivalentTo(expected);
         }
 
         [TestMethod]
-        public void ShouldImport_1_0_12_Default()
+        public void ShouldImport_Profile_1_0_12_Default()
         {
-            string inputFile = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\ImportValues--profile-1.0.12-default--input.json";
-            string expectedFile = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\ImportValues--profile-1.0.12-default--expected.json";
+            string inputFile = PROFILE_DIRECTORY + "ImportValues--profile-1.0.12-default--input.json";
+            string expectedFile = PROFILE_DIRECTORY + "ImportValues--profile-1.0.12-default--expected.json";
             UserSettings.ImportValues(inputFile);
             UserSettings actual = UserSettings.GetInstance();
             actual.KeyboardMappings = null;
             UserSettings expected = ReadExpectedFile(expectedFile);
+
             AssertionExtensions.Should(actual).BeEquivalentTo(expected);
         }
 
         [TestMethod]
-        public void ShouldImport_Empty()
+        public void ShouldImport_Profile_Empty()
         {
-            string inputFile = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\ImportValues--profile-empty--input.json";
-            string expectedFile = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\ImportValues--profile-empty--expected.json";
+            string inputFile = PROFILE_DIRECTORY + "ImportValues--profile-empty--input.json";
+            string expectedFile = PROFILE_DIRECTORY + "ImportValues--profile-empty--expected.json";
             UserSettings.ImportValues(inputFile);
             UserSettings actual = UserSettings.GetInstance();
             actual.KeyboardMappings = null;
-            Console.WriteLine("actual" + actual.Mappings);
             UserSettings expected = ReadExpectedFile(expectedFile);
+
             AssertionExtensions.Should(actual).BeEquivalentTo(expected);
         }
 
+        [TestMethod]
+        public void ShouldImport_Property_AimToggle()
+        {
+            string inputFile = PROFILE_DIRECTORY + "ImportValues--property-AimToggle--input.json";
+            string expectedFile = PROFILE_DIRECTORY + "ImportValues--property-AimToggle--expected.json";
+            UserSettings.ImportValues(inputFile);
+            UserSettings actual = UserSettings.GetInstance();
+            actual.KeyboardMappings = null;
+            UserSettings expected = ReadExpectedFile(expectedFile);
+
+            AssertionExtensions.Should(actual).BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void ShouldImport_Property_AimToggleRetoggleDelay()
+        {
+            string inputFile = PROFILE_DIRECTORY + "ImportValues--property-AimToggleRetoggleDelay--input.json";
+            string expectedFile = PROFILE_DIRECTORY + "ImportValues--property-AimToggleRetoggleDelay--expected.json";
+            UserSettings.ImportValues(inputFile);
+            UserSettings actual = UserSettings.GetInstance();
+            actual.KeyboardMappings = null;
+            UserSettings expected = ReadExpectedFile(expectedFile);
+
+            AssertionExtensions.Should(actual).BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void ShouldImport_Property_MouseAimSensitivityEnabled()
+        {
+            string inputFile = PROFILE_DIRECTORY + "ImportValues--property-MouseAimSensitivityEnabled--input.json";
+            string expectedFile = PROFILE_DIRECTORY + "ImportValues--property-MouseAimSensitivityEnabled--expected.json";
+            UserSettings.ImportValues(inputFile);
+            UserSettings actual = UserSettings.GetInstance();
+            actual.KeyboardMappings = null;
+            UserSettings expected = ReadExpectedFile(expectedFile);
+
+            AssertionExtensions.Should(actual).BeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public void ShouldImport_Property_RemotePlayVolume()
+        {
+            string inputFile = PROFILE_DIRECTORY + "ImportValues--property-RemotePlayVolume--input.json";
+            string expectedFile = PROFILE_DIRECTORY + "ImportValues--property-RemotePlayVolume--expected.json";
+            UserSettings.ImportValues(inputFile);
+            UserSettings actual = UserSettings.GetInstance();
+            actual.KeyboardMappings = null;
+            UserSettings expected = ReadExpectedFile(expectedFile);
+
+            AssertionExtensions.Should(actual).BeEquivalentTo(expected);
+        }
     }
 }
