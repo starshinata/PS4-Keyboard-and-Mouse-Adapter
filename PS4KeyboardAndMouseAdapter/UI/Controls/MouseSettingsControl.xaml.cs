822c0498aaad6852b38e2abd5c3bcec34b7f1435
pancakeslp <pancakeslp@gmail.com>
2021-01-14 00:03:03 +0000

    MouseSettingsControl.xaml.cs removing unused import

--- PS4KeyboardAndMouseAdapter/UI/Controls/MouseSettingsControl.xaml.cs
+++ PS4KeyboardAndMouseAdapter/UI/Controls/MouseSettingsControl.xaml.cs
@@ -1,11 +1,9 @@
-﻿using PS4KeyboardAndMouseAdapter.Config;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace PS4KeyboardAndMouseAdapter.UI.Controls
 {
     public partial class MouseSettingsControl : UserControl
     {
-
         public MouseSettingsControl()
         {
             InitializeComponent();
