399b0204f8678d6c4bb4305c065e8f8c5d04c329
pancakeslp <pancakeslp@gmail.com>
2021-02-28 00:47:16 +0000

    updating version number, and making it so we only need to update version number in one place (build.ps1)

--- PS4KeyboardAndMouseAdapter/Properties/AssemblyInfo.cs
+++ PS4KeyboardAndMouseAdapter/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -46,9 +46,7 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-
+// assume the below values are GENERATED
+// see build.ps1 for version management
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
