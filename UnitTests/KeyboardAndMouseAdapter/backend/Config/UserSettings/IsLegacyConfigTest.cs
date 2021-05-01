83779e30c22bd8758fa8cde8a299ed654fb8635c
pancakeslp <pancakeslp@gmail.com>
2021-05-01 01:21:54 +0100

    updated unit tests

--- UnitTests/KeyboardAndMouseAdapter/backend/Config/UserSettings/IsLegacyConfigTest.cs
+++ UnitTests/KeyboardAndMouseAdapter/backend/Config/UserSettings/IsLegacyConfigTest.cs
@@ -7,9 +7,10 @@ namespace UnitTests.KeyboardAndMouseAdapter.Config.UserSettingsTest
     [TestClass]
     public class IsLegacyConfigTest
     {
-        private readonly string PROJECT_ROOT ="..\\..\\..\\";
+        private readonly string PROJECT_ROOT = "..\\..\\..\\";
 
-        private void IsLegacyConfigRunner( string file, bool expected) {
+        private void IsLegacyConfigRunner(string file, bool expected)
+        {
             string json = File.ReadAllText(file);
             bool actual = UserSettings.IsLegacyConfig(json);
             Assert.AreEqual(expected, actual);
@@ -18,14 +19,14 @@ namespace UnitTests.KeyboardAndMouseAdapter.Config.UserSettingsTest
         [TestMethod]
         public void ShouldDetect_1_0_11_IsLegacy()
         {
-            string file = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\IsLegacyConfig--profile-1.0.11-default.json";
+            string file = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\backend\\Config\\UserSettings\\IsLegacyConfig--profile-1.0.11-default.json";
             IsLegacyConfigRunner(file, true);
         }
 
         [TestMethod]
         public void ShouldDetect_2_0_0_IsCurrent()
         {
-            string file = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\Config\\UserSettings\\IsLegacyConfig--profile-2.0.0-provisional.json";
+            string file = PROJECT_ROOT + "UnitTests\\KeyboardAndMouseAdapter\\backend\\Config\\UserSettings\\IsLegacyConfig--profile-2.0.0-provisional.json";
             IsLegacyConfigRunner(file, false);
         }
     }
