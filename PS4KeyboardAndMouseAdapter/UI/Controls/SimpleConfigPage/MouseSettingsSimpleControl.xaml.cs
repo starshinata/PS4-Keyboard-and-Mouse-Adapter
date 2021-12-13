c65784c984bef71e76dbecc805ac80d227fe593d
pancakeslp <pancakeslp@gmail.com>
2021-12-13 22:14:16 +0000

    3.0.0 wip

---
+++ PS4KeyboardAndMouseAdapter/UI/Controls/SimpleConfigPage/MouseSettingsSimpleControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PS4KeyboardAndMouseAdapter.Config;
+using Serilog;
+using System.Windows.Controls;
+
+namespace PS4KeyboardAndMouseAdapter.UI.Controls
+{
+    public partial class MouseSettingsSimpleControl : UserControl
+    {
+        public MouseSettingsSimpleControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Log.Information("MouseSettingsSimpleControl.sendX");
+            Log.Information("InstanceSettings.GetInstance().GetVigemInjector()");
+            Log.Information(InstanceSettings.GetInstance().GetVigemInjector().ToString());
+            InstanceSettings.GetInstance().GetVigemInjector().sendX();
+        }
+    }
+}
