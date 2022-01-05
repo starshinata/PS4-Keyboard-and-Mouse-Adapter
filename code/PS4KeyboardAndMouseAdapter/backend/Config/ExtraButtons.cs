fba79898ba6199ad777a1ec754a297cc7547fcdb
pancakeslp <pancakeslp@gmail.com>
2022-01-05 12:37:49 +0000

    3.0.0 wip -  fixing namespaces

--- code/PS4KeyboardAndMouseAdapter/backend/Config/ExtraButtons.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pizza.KeyboardAndMouseAdapter.Backend.Config
-{
-    public enum ExtraButtons
-    {
-        Unknown = -1,
-        MouseWheelUp = 0,
-        MouseWheelDown = 1
-    }
-}
--- code/PS4KeyboardAndMouseAdapter/backend/RemotePlayConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pizza.KeyboardAndMouseAdapter.Backend
-{
-    public class RemotePlayConstants
-    {
-        public static readonly string INJECT_DLL_NAME = "PS4RemotePlayInjection.dll";
-        public static readonly string TARGET_PROCESS_NAME = "RemotePlay";
-        public static readonly string WINDOW_NAME = "PS Remote Play";
-    }
-}
