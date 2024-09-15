9c738b43e146d5eb9e241cdfe90924b22ca7f14f
pancakeslp <pancakeslp@gmail.com>
2024-09-15 12:16:55 +0100

    Forgot to version bump the last release

--- build.ps1
+++ build.ps1
@@ -29,7 +29,7 @@ $CERT_DIRECTORY = "E:\workspace\##certificates\github.com-pancakeslp"
 #$MS_BUILD_CONFIG = "Debug"
 $MS_BUILD_CONFIG = "Release"
 
-$VERSION = "3.1.1"
+$VERSION = "3.1.2"
 
 ################################
 ################################
