d037fb330bc788fb4b11451ec2e96dd3de4f32d0
pancakeslp <pancakeslp@gmail.com>
2026-01-04 01:23:00 +0000

    injector progressing

--- code/RemotePlayInjected/EntryPoint.cs
+++ code/RemotePlayInjected/EntryPoint.cs
@@ -13,12 +13,12 @@ namespace RemotePlayInjected
     {
 
        public EntryPoint(EasyHook.RemoteHooking.IContext context)
-        {
+       {
             LogManager logManager = new LogManager();
             logManager.SetupLogger();
 
             Log.Information("HI from `code/RemotePlayInjected/EntryPoint.cs' constructor");
-        }
+       }
 
         /// <summary>
         /// The main entry point for our logic once injected within the target process.
@@ -28,16 +28,24 @@ namespace RemotePlayInjected
         /// <param name="context">The RemoteHooking context</param>
         public void Run(EasyHook.RemoteHooking.IContext context)
         {
+            Log.Information("EntryPoint.Run ref31");
+
+            ThreadRpcUpdateListener listener ;
             try
             {
-                Log.Information("EntryPoint.Run ref30");
-                ThreadRpcUpdateListener listener = new ThreadRpcUpdateListener();
-                Log.Information("EntryPoint.Run ref32");
-                listener.DoWork();
-                Log.Information("EntryPoint.Run ref34");
+                 listener = new ThreadRpcUpdateListener();
             }
             catch (Exception e) {
-                ExceptionLogger.LogException("EntryPoint.Run error ref 40", e);
+                ExceptionLogger.LogException("EntryPoint.Run RPC startup error", e);
+                return;
+            }
+
+            try
+            {
+                listener.DoWork();
+            }
+            catch (Exception e) {
+                ExceptionLogger.LogException("EntryPoint.Run RPC DoWork error", e);
             }
         }
 
